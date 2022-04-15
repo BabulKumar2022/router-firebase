944a16ce8205befddc9e4e20a8cfc80ccf944a30
BabulKumar2022 <bb3583400@gmail.com>
2022-04-16 01:06:28 +0600

    navlink added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Random</a>
+            <Link className="navbar-brand" to='/'>Random</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarText">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                <a className="nav-link " aria-current="page" href="#">Home</a>
+                <Link className="nav-link " aria-current="page" to="/home">Home</Link>
                 </li>
                 <li className="nav-item">
-                <a className="nav-link" href="#">Users</a>
+                <Link className="nav-link" to="/users">Users</Link>
                 </li>
                 <li className="nav-item">
-                <a className="nav-link" href="#">Login</a>
+                <Link className="nav-link" to="/login">Login</Link>
                 </li>
             </ul>
             <span className="navbar-text">
